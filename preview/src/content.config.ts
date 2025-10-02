2c808f1caf29ba218b010e1808c357fb3df5b5dd
Niklas Mollenhauer <nikeee@outlook.com>
2025-10-02 02:59:05 +0200

    Add basic page

--- preview/src/content.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineCollection, z, type CollectionEntry } from "astro:content";
-import { glob } from "astro/loaders";
-
-const samples = defineCollection({
-	loader: glob({ pattern: "*.hurl", base: "../test" }),
-});
-
-export const collections = { samples };
